a9f4d37298701d724e4cca864d56e5e1e9fd9ab8
Mohan Krishna <mohankrishna130101@gmail.com>
2022-09-20 20:17:56 +0530

    Alteres

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const cacheName = 'pwa-conf-myapp';
+const cacheName = "pwa-conf-v1";
 const staticAssets = [
-  './',
-  './index.html',
-  './app.js',
-  './styles.css'
+  "./",
+  "./index.html",
+  "./app.js",
+  "./styles.css",
 ];
 
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName); 
-  await cache.addAll(staticAssets); 
+self.addEventListener("install", async (event) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
 });
-
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   const req = event.request;
   event.respondWith(cacheFirst(req));
 });
 
 async function cacheFirst(req) {
-  const cache = await caches.open(cacheName); 
-  const cachedResponse = await cache.match(req); 
-  return cachedResponse || fetch(req); 
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || fetch(req);
 }
